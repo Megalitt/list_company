d5b1a2a9d424137607b393e8011d321b373bb81e
Андрей Шастин <97834468+Megalitt@users.noreply.github.com>
2023-04-24 20:06:26 +0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,181 @@
+export const data = {
+  company: {
+    title: 'Компании',
+    company: 'Название компании',
+    count: 'Количество сотрудников',
+    adres: 'Адрес'
+  },
+  employees: {
+    title: 'Сотрудники',
+    surname: 'Фамилия',
+    name: 'Имя',
+    job: 'Должность'
+  }
+};
+
+export const compani = [
+  {
+    checkedCompany: false,
+    id: 1,
+    nameCompany: 'Solo',
+    number: 0,
+    adres: 'strid, 5',
+    employees: [
+      {
+        checkedEmploye: false,
+        id: 34,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 35,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 36,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 37,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 17,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+    ] 
+  },
+  {
+    checkedCompany: false,
+    id: 2,
+    nameCompany: 'Solo',
+    number: 0,
+    adres: 'strid, 5',
+    employees: [
+      {
+        checkedEmploye: false,
+        id: 52,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 53,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 54,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+    ] 
+  },
+  {
+    checkedCompany: false,
+    id: 3,
+    nameCompany: 'Solo',
+    number: 0,
+    adres: 'strid, 5',
+    employees: [
+      {
+        checkedEmploye: false,
+        id: 60,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 61,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 62,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 63,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 69,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 67,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+    ] 
+  },
+  {
+    checkedCompany: false,
+    id: 4,
+    nameCompany: 'Solo',
+    number: 0,
+    adres: 'strid, 5',
+    employees: [
+      {
+        checkedEmploye: false,
+        id: 41,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 42,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 40,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+      {
+        checkedEmploye: false,
+        id: 39,
+        surName: 'Иванов',
+        name: 'Иван',
+        job: 'Плотник'
+      },
+    ] 
+  },
+ 
+ 
+]
---
+++ src/store/companyReduser.js
@@ -0,0 +1,98 @@
+
+const defauState = {
+    companies: [
+      {
+        checkedCompany: false,
+        id: Date.now(),
+        nameCompany: '',
+        number: 0,
+        adres: '',
+        employees: [
+          {
+            checkedEmploye: false,
+            id: Date.now(),
+            surName: '',
+            name: '',
+            job: ''
+          }
+        ]
+      }
+    ]
+};
+
+const ADD_COMPANY = "ADD_COMPANY";
+const ADD_EMPLOYESS = "ADD_EMPLOYESS";
+const REMOVE_COMPANY = "REMOVE_COMPANY";
+const REMOVE_EMPLOYES = "REMOVE_EMPLOYES";
+const ADD_LIST = "ADD_LIST";
+const CHECK_COMPANY = "CHECK_COMPANY";
+const CHECK_COMPANY_ALL = "CHECK_COMPANY_ALL";
+const CHECK_EMPLOYE = "CHECK_EMPLOYE";
+const CHECK_EMPLOYE_ALL = "CHECK_EMPLOYE_ALL";
+
+
+export const companyReduser = (state = defauState, action) => {
+  switch(action.type){
+    case ADD_COMPANY:
+      return {
+        ...state,
+        companies: [...state.companies, action.payload]
+      }
+    case ADD_EMPLOYESS:
+      return {
+        ...state,
+        ...state.companies[action.payload[0].checkeds.indexCom].employees.push(action.payload[0].employe) 
+      }
+    case ADD_LIST:
+      return {
+        ...state,
+        companies: [...state.companies, ...action.payload]
+      }
+    case CHECK_COMPANY_ALL:
+      return {
+        ...state,
+        ...state.companies[action.payload.i].checkedCompany = action.payload.checked
+      }
+    case CHECK_EMPLOYE_ALL:
+      return {
+        ...state,
+        ...state.companies[action.payload.i].employees[action.payload.j].checkedEmploye = action.payload.checked
+      }
+    case CHECK_COMPANY:
+      return {
+        ...state, 
+        ...state.companies[action.payload.index].checkedCompany = action.payload.checked
+      }
+    case CHECK_EMPLOYE:
+      const {indexCom, idEmp, checked} = action.payload;
+      return {
+        ...state,
+        ...state.companies[action.payload.indexCom].employees[action.payload.indexEmp].checkedEmploye = action.payload.checked       
+      }
+    case REMOVE_COMPANY:
+      return {
+        ...state,
+          companies: state.companies.filter(com => com.id !== action.payload)
+      }
+    case REMOVE_EMPLOYES:
+      console.log(state.companies[action.payload.indexCom].employees.filter(emp => emp.id !== action.payload.idEmp))  //кнопка delete
+      
+      return {
+        ...state,
+        ...state.companies[action.payload.indexCom].employees = state.companies[action.payload.indexCom].employees.filter(emp => emp.id !== action.payload.idEmp)  //кнопка delete
+      }
+    default:
+      return state;
+  }
+};
+
+export const checkCompanyAction = (payload) => ({type: CHECK_COMPANY, payload});
+export const removeCompanyAction = (payload) => ({type: REMOVE_COMPANY, payload});
+export const removeEmployesAction = (payload) => ({type: REMOVE_EMPLOYES, payload});
+export const checkAllCompanyAction = (payload) => ({type: CHECK_COMPANY_ALL, payload});
+export const checkAllEmployesAction = (payload) => ({type: CHECK_EMPLOYE_ALL, payload});
+export const checkEmployeAction = (payload) => ({type: CHECK_EMPLOYE, payload});
+export const addCompanyAction = (payload) => ({type: ADD_COMPANY, payload});
+export const addEmploeysAction = (payload) => ({type: ADD_EMPLOYESS, payload});
+export const addListAction = (payload) => ({type: ADD_LIST, payload});
+
